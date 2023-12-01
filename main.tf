ed3ca475009c8b25014d74147fb0560e29db60f2
tabbertj <101358806+tabbertj@users.noreply.github.com>
2023-12-01 07:54:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,6 +50,13 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = [var.subnet_prefix]
 }
 
+data "azurerm_subnet" "subnet" {
+  name                 = "${var.prefix}-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.myresourcegroup.name
+}
+
+
 resource "azurerm_network_security_group" "catapp-sg" {
   name                = "${var.prefix}-sg"
   location            = var.location
